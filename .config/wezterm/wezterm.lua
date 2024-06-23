9cbfb01303ab7402540c52fea428914ed9cc1f51
wawter <waterexit1120@gmail.com>
2024-06-23 19:06:03 +0900

    first commit

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require("wezterm")
+local act = wezterm.action
+
+return {
+	color_scheme = "Sakura",
+	font = wezterm.font_with_fallback({
+		-- F0001 -> text?
+		{ family = "chiikawa", assume_emoji_presentation = false },
+		{ family = "chiikawa", assume_emoji_presentation = true },
+	}),
+	font_size = 13.0,
+  allow_square_glyphs_to_overflow_width = "Never",
+}
