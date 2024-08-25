37fa09a961c1b000a22e07c8a8d9d98e9d8331f1
water <water@dummy.com>
2024-08-25 15:03:29 +0900

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 function install {
-  if $(which $1); then
+  if (which $1) ; then
+    echo "$1 is arleady installed"
+    return
+  fi
+  echo "$1 installing..."
 	if [ -p /dev/stdin ];then
 		cat -
 	else
-		$2
-	fi | source
-  fi
+		echo $2
+	fi | bash
 }
 
 install nvim <<EOS
 	curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz &&
 	sudo rm -rf /opt/nvim && 
-	sudo tar -C /opt -xzf nvim-linux64.tar.gz &&
-	sudo rm nvim-linux64.tar.gz
+	sudo tar -C /opt -xzf nvim-linux64.tar.gz
+	# sudo rm nvim-linux64.tar.gz
 EOS
