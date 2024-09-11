93c986ed453fd372d3eebf7628aa487454312063
water <water@dummy.com>
2024-09-11 19:45:53 +0900

    set up fix

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
 
 dotfile_dir=$(cd $(dirname '$1') && pwd) 
-
-replace_dot_files=(".bashrc" ".bash_profile" ".bash_aliases" ".config/nvim", ".config/wezterm")
-for file in ${replace_dot_files[@]};do
+replace_dot_files=(".bashrc" ".bash_profile" ".bash_aliases")
+for file in ${replace_dot_files[@]} $(ls -d .config/*);do
   if [ -f ~/${file} ] && ! [ -L ~/${file} ]; then
     mv ~/${file} ~/${file}.bk
   fi
