318ac3a2f41e09468598f227abce69e173b145bf
water <water@dummy.com>
2024-11-17 19:48:01 +0900

    replace nvchad simple nvim settings

---
+++ .config/nvim/lua/mapping.lua
@@ -0,0 +1,2 @@
+local map = vim.api.nvim_set_keymap
+map("i","jj","<ESC>",{})
---
+++ .config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,9 @@
+local M = {
+    "nvim-treesitter/nvim-treesitter",
+    build = function()
+        require("nvim-treesitter.install").update({ with_sync = true })()
+    end,
+}
+
+return { M }
+
