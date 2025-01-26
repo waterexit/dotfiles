a910c45ab02b1a3c9d74e588f31e1ef5ea06fd62
water <waterexit1120@gmail.com>
2025-01-26 12:40:22 +0900

    add vim setting undo_history & opacity

--- .config/nvim/lua/setting/vim.lua
+++ .config/nvim/lua/setting/vim.lua
@@ -1,10 +1,28 @@
+local stdpath = vim.fn.stdpath("data")
+
+-- アンドゥ履歴用ディレクトリを設定
+local undodir = stdpath .. "/undo"
+vim.o.undodir = undodir
+vim.o.undofile = true
+vim.fn.mkdir(undodir, "p")
+
 -- NOTE: "filetype on" command should executed after dpp.vim change runtimepath.
 -- if this line is at the firstline, filetype is empty.
 -- probably, the reason "filetype on" searches tree-sitter setting from rutimepath.
-vim.cmd[[
+vim.cmd ([[
 	filetype indent plugin on
 	syntax on
 	set number
 	set expandtab
 	set shiftwidth=4
-]]
+]])
+
+-- 透明にする
+vim.cmd([[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+  highlight NormalNC guibg=none
+  highlight NormalSB guibg=none
+]])
